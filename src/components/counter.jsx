bc58ed3fdc1357a6ff0219cd143759a9d2ee972d
JuanSantillan97 <juanyut97@hotmail.com>
2019-05-07 11:11:44 -0500

    Counters y Delete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,39 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   state = {  
-    count: 0,
+    value: this.props.counter.value,
     imageUrl: "https://picsum.photos/200",
     tags: ['tag1', 'tag2', 'tag3']
+  }
+
+  handleIncrement = product => {
+    console.log(product)
+    this.setState({ value: this.state.value + 1 })
+  }
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1})
   }
 
   render() { 
     return (
       <div>
+        { this.props.children }
         <img className="m-2" src={ this.state.imageUrl } alt=""></img>
 
         <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <button 
+          onClick={ () => this.doHandleIncrement() }
+          className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+
+        <button
+          onClick={ () => this.props.onDelete(this.props.counter.id) }
+          className="btn btn-danger btn-sm m-2">
+          Delete
+        </button>
 
         <ul>
           { this.state.tags.map( tag => <li key={ tag }>{ tag }</li> ) }
@@ -29,8 +50,8 @@ class Counter extends Component {
   }
 
   formatCount() {
-    const { count } = this.state
-    return  count === 0 ? "Zero" : count
+    const { value } = this.state
+    return  value === 0 ? "Zero" : value
   }
 
 }
